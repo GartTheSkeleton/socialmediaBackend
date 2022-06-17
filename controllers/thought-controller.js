fb7ae643a69e34ba86566ac5d2ea30c2b287ce1e
Gart <gmhobson@gmail.com>
2022-06-16 19:40:12 -0500

    Full CRUD for thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -28,6 +28,44 @@ const thoughtController = {
             res.json(dbUserData);
           })
         .catch(err => res.status(400).json(err));
+    },
+
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+        .then (deletedThought => {
+            if (!deletedThought) {
+                return res.status(404).json({message: "No Thought with this ID."})
+            }
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.thoughtId }},
+                { new: true }
+            )
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json( {message: 'No user found with this ID.'})
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err))
+    },
+
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId},
+            {thoughtText: body.thoughtText},
+            { new: true }
+            )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json( { message: "No Thought with this ID" } )
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => res.json(err))
     }
 }
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 
-const { getAllThoughts, createThought } = require('../../controllers/thought-controller');
+const { getAllThoughts, createThought, removeThought, updateThought } = require('../../controllers/thought-controller');
 
 router
     .route('/')
     .get(getAllThoughts)
 
 router.route('/:userId').post(createThought);
+
+router
+  .route('/:userId/:thoughtId')
+  .put(updateThought)
+  .delete(removeThought)
 
 module.exports = router;
